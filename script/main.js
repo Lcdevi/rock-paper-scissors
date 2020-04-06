7e23876f71832daf67f9e53c27509c51b1d2e392
JMSA <jm.staana@outlook.com>
2020-04-06 22:54:51 +1200

    HTML for the game is added

---
+++ script/main.js
@@ -0,0 +1,56 @@
+const handChoices = ["rock","paper","scissors"];
+let playerScore = 0;
+let computerScore = 0;
+let round = 0;
+let result = "";
+
+function game(){
+    let player ="";
+    for(let i = 0; i <= 4; i++){
+        player = prompt("Please type your choice, Rock,Paper, or Scissors").toLowerCase();
+        while(!checkPlayerInput(player)){
+            alert("Please stop breaking my game, Im just a girl, standing in front of a computer!!!");
+            player = prompt("Please type your choice, Rock,Paper, or Scissors").toLowerCase();
+        }
+        playRound(player,computerPlay());
+        console.log(`Round: ${round}
+                     Scores Player: ${playerScore} - CPU: ${computerScore}
+                     Match: ${result}`
+        );
+    }
+    reset();
+}
+
+function checkPlayerInput(input){
+    return ((input === "") || (handChoices.includes(input)));
+}
+
+function computerPlay(){
+    return handChoices[Math.floor(Math.random() * handChoices.length)];;
+}
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == computerSelection){
+        result = "Its a TIE!!";
+    } else if( (playerSelection == "rock" && computerSelection == "scissors") || 
+               (playerSelection == "paper" && computerSelection == "rock") || 
+               (playerSelection == "scissors" && computerSelection == "paper") ){
+                   
+        result = `You win ${playerSelection} beats ${computerSelection}`;
+        ++playerScore;
+    } else {
+        result = `You lose!! ${computerSelection} beats ${playerSelection}`
+        ++computerScore;;
+    }
+    round++;
+    return result;
+}
+
+function reset(){
+    playerScore = 0;
+    computerScore = 0;
+    round = 0;
+    result = "";
+}
+
+game();
