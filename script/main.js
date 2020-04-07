8048c9741bf28bd216592768025f11706b2d1be3
JMSA <jm.staana@outlook.com>
2020-04-07 18:45:41 +1200

    Dom to html function added(scoreboard,game status and restart button)

--- script/main.js
+++ script/main.js
@@ -1,148 +1,50 @@
-const handChoices = ["Rock","Paper","Scissors"];
-let playerScore = 0;
-let computerScore = 0;
-let rounds = 0;
-
-// Get player hand choice
+// Global variable declaration
+const handChoices = ["Rock", "Paper", "Scissors"];
+const playerScore = document.querySelector('.gameDetails #player');
+const computerScore = document.querySelector('.gameDetails #computer');
+const gameRound = document.querySelector('.rounds #round');
+const result = document.querySelector('.gameStatus h2');
 const getHands = document.querySelector('.handCategories');
-getHands.addEventListener('click',function (e) {
-    const player = e.target.id;
-    console.log(player);
+
+getHands.addEventListener('click', function (e) {
+    player = e.target.id;
+    gameInit(player);
 });
 
 // Get computer choice by random index selection in the hand arr choices
-function computer() {
+function computerInput() {
     return Math.floor(Math.random() * handChoices.length);
 }
 
 // Compare the round against the player input and computer
-function playRound(playerInput, computerInput){
-    let result = "";
-    if(playerInput == computerInput){
-        result = "It's a TIE!!!";
-    } else if( (playerInput == 0 && computerInput == 2) || 
-               (playerInput == 1 && computerInput == 0) ||
-               (playerInput == 2 && computerInput == 1) ){
-      result = `You win !! ${handChoices[playerInput]} beats ${handChoices[computerInput]}`;
-      playerScore++;             
+function playRound(playerInput, computerInput) {
+    gameRound.textContent = parseInt(gameRound.textContent, 10) + 1;
+    if (playerInput == computerInput) {
+        result.textContent = "It's a TIE!!!";
+    } else if ((playerInput == 0 && computerInput == 2) ||
+        (playerInput == 1 && computerInput == 0) ||
+        (playerInput == 2 && computerInput == 1)) {
+        result.textContent = `You win !! ${handChoices[playerInput]} beats ${handChoices[computerInput]}`;
+        playerScore.textContent = parseInt(playerScore.textContent, 10) + 1;
     } else {
-      result = `You lose !! ${handChoices[computerInput]} beats ${handChoices[playerInput]}`;
-      computerScore++;
+        result.textContent = `You lose !! ${handChoices[computerInput]} beats ${handChoices[playerInput]}`;
+        computerScore.textContent = parseInt(computerScore.textContent, 10) + 1;
     }
-    rounds++;
+    console.log(gameRound.textContent);
 }
 
+// Initialize the game and apply the dom styling in the html
+function gameInit(player) {
+    if (Number(gameRound.textContent) === 5) {
+        
+        isWinner();
+    } else {
+        playRound(player, computerInput());
+    }
+}
 
-
-
-
-// function playRound(playerSelection, computerSelection){
-//     if(playerSelection == computerSelection){
-//         result = "Its a TIE!!";
-//     } else if( (playerSelection == "rock" && computerSelection == "scissors") || 
-//                (playerSelection == "paper" && computerSelection == "rock") || 
-//                (playerSelection == "scissors" && computerSelection == "paper") ){
-                   
-//         result = `You win ${playerSelection} beats ${computerSelection}`;
-//         ++playerScore;
-//     } else {
-//         result = `You lose!! ${computerSelection} beats ${playerSelection}`
-//         ++computerScore;;
-//     }
-//     round++;
-//     return result;
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const handChoices = ["rock","paper","scissors"];
-// let playerScore = 0;
-// let computerScore = 0;
-// let round = 0;
-// let result = "";
-
-// function game(){
-//     let player ="";
-//     for(let i = 0; i <= 4; i++){
-//         player = prompt("Please type your choice, Rock,Paper, or Scissors").toLowerCase();
-//         while(!checkPlayerInput(player)){
-//             alert("Please stop breaking my game, Im just a girl, standing in front of a computer!!!");
-//             player = prompt("Please type your choice, Rock,Paper, or Scissors").toLowerCase();
-//         }
-//         playRound(player,computerPlay());
-//         console.log(`Round: ${round}
-//                      Scores Player: ${playerScore} - CPU: ${computerScore}
-//                      Match: ${result}`
-//         );
-//     }
-//     reset();
-// }
-
-// function checkPlayerInput(input){
-//     return ((input === "") || (handChoices.includes(input)));
-// }
-
-// function computerPlay(){
-//     return handChoices[Math.floor(Math.random() * handChoices.length)];;
-// }
-
-// function playRound(playerSelection, computerSelection){
-//     if(playerSelection == computerSelection){
-//         result = "Its a TIE!!";
-//     } else if( (playerSelection == "rock" && computerSelection == "scissors") || 
-//                (playerSelection == "paper" && computerSelection == "rock") || 
-//                (playerSelection == "scissors" && computerSelection == "paper") ){
-                   
-//         result = `You win ${playerSelection} beats ${computerSelection}`;
-//         ++playerScore;
-//     } else {
-//         result = `You lose!! ${computerSelection} beats ${playerSelection}`
-//         ++computerScore;;
-//     }
-//     round++;
-//     return result;
-// }
-
-// function reset(){
-//     playerScore = 0;
-//     computerScore = 0;
-//     round = 0;
-//     result = "";
-// }
-
-// game();
+function isWinner() {
+    (Number(playerScore.textContent) > Number(computerScore.textContent)) ? result.textContent = "You win the game! Congratulations!" :
+        (Number(computerScore.textContent) > Number(playerScore.textContent)) ? result.textContent = "You lost the game! against a bot!" :
+            result.textContent = "Its a TIE!!!!"
+}
